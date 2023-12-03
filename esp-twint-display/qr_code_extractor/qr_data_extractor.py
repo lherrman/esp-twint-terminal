de10b21d59c03d16d75be746a81834af495fd8a6
lherrman <herrmann.lars@gmx.ch>
2023-12-03 17:38:16 +0100

    Refactor QR code extraction and add default QR code support

--- esp-twint-display/qr_code_extractor/qr_data_extractor.py
+++ esp-twint-display/qr_code_extractor/qr_data_extractor.py
@@ -22,10 +22,14 @@ def delete_files(path, extension):
 
 def convert_pdf_to_png(pdf_file_path):
     # Read Text and Convert to Image
+    name = os.path.basename(pdf_file_path).split('.')[0]
+    default_code = name == 'default'
+
     reader = PdfReader(pdf_file_path)    
     page = reader.pages[0]
-    value, store_name = page.extract_text().splitlines()[0:2]
-    value = float(value.split(' ')[-1])
+    if not default_code:
+        value, store_name = page.extract_text().splitlines()[0:2]
+        value = float(value.split(' ')[-1])
     image = convert_from_path(pdf_file_path, poppler_path=poppler_path)[0]
 
     # Crop Image to QR Code Area
@@ -37,8 +41,12 @@ def convert_pdf_to_png(pdf_file_path):
     # image = image.convert('1')
 
     # Save Image
-    value = int(value * 100)
-    file_name = f'qr{value:05}.png'
+    if not default_code:
+        value = int(value * 100)
+        file_name = f'qr{value:05}.png'
+    else:
+        file_name = f'default.png'
+
     path = f'{img_dest_dir}/{file_name}'
     image.save(f'{path}', 'PNG')
     print(f'{file_name} saved')
@@ -58,14 +66,22 @@ def main():
     print('All qr codes extracted')
 
     # read the qr codes data
-    data = {}
+    qr_data = {}
+    qr_data_default = ''
     for img in images_paths:
         file_name = os.path.basename(img).split('.')[0]
+        if file_name == 'default':
+            qr_data_default = decode(Image.open(img))[0].data.decode('utf-8')
+            continue
+
         file_name = re.sub('[^0-9]', '', file_name)
-        price = int(file_name)
+        key = int(file_name) 
         qrdata = decode(Image.open(img))[0].data.decode('utf-8')
-        data[price] = qrdata
+        qr_data[key] = qrdata
     print('All qr codes read')
+
+    if qr_data_default == '':
+        raise Exception('No default qr code found. Make sure there is a pdf file named default.pdf in the source directory')
 
     # construct the cpp and h files
     function_declaration = 'const char * get_qr_data(float price)'
@@ -76,12 +92,12 @@ def main():
     cpp_file_content += '    int price_switch = (int)(price * 100);\n'
     cpp_file_content += '    switch (price_switch)\n'
     cpp_file_content += '    {\n'
-    for price, value in data.items():
+    for price, value in qr_data.items():
         cpp_file_content += f'        case {price}:\n'
         cpp_file_content += f'            return "{value}";\n'
         cpp_file_content += '             break;\n'
     cpp_file_content += '        default:\n'
-    cpp_file_content += '            return "02:68fc89929d6348d9aa6d270a1425aa65#928942409ead76410e713bcb7cf3f5bd66edefad#";\n' # TODO: change this to a default qr code (currently UNVALID)
+    cpp_file_content += f'            return "{qr_data_default}";\n' # TODO: change this to a default qr code (currently UNVALID)
     cpp_file_content += '            break;\n'
     cpp_file_content += '    }\n'
     cpp_file_content += '}\n'
