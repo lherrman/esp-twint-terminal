a25e33ea839842cdff005d04df8b73205db71058
lherrman <herrmann.lars@gmx.ch>
2023-12-03 17:43:55 +0100

    Refactor folder extraction script to move files from subdirectories to working directory

--- esp-twint-display/qr_code_extractor/folder_extractor.py
+++ esp-twint-display/qr_code_extractor/folder_extractor.py
@@ -1,4 +1,7 @@
-# Script to move all files from the subdirectories of a directory to the directory itself and delete the subdirectories:
+# Script to move all files from the subdirectories of a 
+# directory to the directory itself and delete the subdirectories:
+#
+# All files in the subdirectories of the working directory will be moved to the working directory.
 
 import os
 import shutil
